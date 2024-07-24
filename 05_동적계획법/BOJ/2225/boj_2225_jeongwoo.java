22b0624b7c64e117f61b7ba95a53f721148b2a35
jwkim0405 <jeongwoocode@gmail.com>
2024-07-25 01:12:58 +0900

    0724 jeongwoo 해냄

--- 05_동적계획법/BOJ/2225/boj_2225_jeongwoo.java
+++ 05_동적계획법/BOJ/2225/boj_2225_jeongwoo.java
@@ -1,40 +1,38 @@
 package boj;
 
 import java.util.Scanner;
-import java.lang.Math;
 
-// 자리수가 커서 문제 발생
+public class boj_2225_jeongwoo_dp {
+    public static int comb_with_rep(int[][] sum, int n, int k) {
+        if (n==0)
+            return 1;
+        else if (n == 1)
+            return k;
+        else {
+            for (int i = 1; i <= n; i++)
+                sum[i][1] = 1;   // 1개의 카드로 숫자 i를 만드는 경우는 1가지!
 
-public class boj_2225_jeongwoo {
-//    public static long factorial(int n) {
-//        long res = 1L;
-//        if (n >= 2)
-//            for (int i = 2; i <= n; i++)
-//                res *= i;
-//        return res;
-//    }
-    public static long comb_rep(int N, int K) {
-        long res = 1;
-        long max_divisor = Math.max(N, K-1);
-        long min_divisor = Math.min(N, K-1);
+            for (int i = 1; i <= k; i++) {
+                sum[0][i] = 1;  // i개의 카드로 0 만들기
+                sum[1][i] = i;  // i개의 카드로 1 만들기
+            }
 
-        for (int i = K+N-1; i > max_divisor; i--)
-            res *= i;
-
-
-        for (int i = 2; i <= min_divisor; i++)
-            res /= i;
-
-        return res % 1000000000;
+            // k먼저 고정
+            for (int i = 2; i <= k; i++)
+                for (int j = 2; j <= n; j++)
+                    for (int l = 0; l <= j; l++)
+                        sum[j][i] = (sum[j][i] + sum[l][i - 1]) % 1000000000;
+            return sum[n][k];
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
-//        long res = factorial(K+N-1);
+        int[][] sum = new int[n+1][k+1];
+        System.out.print(comb_with_rep(sum, n, k));
 
-        System.out.print(comb_rep(N, K));
     }
 }
