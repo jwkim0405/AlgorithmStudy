af4c2867a472667cefdddfa3da7e5dd9c496413e
hyenem <gpals04291@gmail.com>
2024-08-07 00:16:24 +0900

    0807

--- 00_기초문제/BOJ/17413/boj_17413_hyenem.java
+++ 00_기초문제/BOJ/17413/boj_17413_hyenem.java
@@ -5,26 +5,26 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String ans = "";
+        StringBuilder stringbuilder = new StringBuilder();
         
         Stack<Character> stack = new Stack<>();
         
         for(int i = 0; i<str.length(); i++){
             if(str.charAt(i)=='<'){
-                while(!stack.isEmpty()) ans+=stack.pop();
+                while(!stack.isEmpty()) stringbuilder.append(stack.pop());
                 while(str.charAt(i)!='>'){
-                    ans+=str.charAt(i);
+                    stringbuilder.append(str.charAt(i));
                     i++;
                 }
-                ans+=str.charAt(i);
+                stringbuilder.append(str.charAt(i));
             } else if(str.charAt(i)==' '){
-                while(!stack.isEmpty()) ans+=stack.pop();
-                ans+=' ';
+                while(!stack.isEmpty()) stringbuilder.append(stack.pop());
+                stringbuilder.append(' ');
             } else {
                 stack.push(str.charAt(i));
             }
         }
-        while(!stack.isEmpty()) ans+=stack.pop();
-        System.out.println(ans);
+        while(!stack.isEmpty()) stringbuilder.append(stack.pop());
+        System.out.println(stringbuilder);
     }
 }
