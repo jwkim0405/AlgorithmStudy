3bf0e81148a4753c90cd6adc7b3f34acbc75f76a
shinyou28 <shinyou0828@gmail.com>
2024-08-12 22:58:29 +0900

    Update boj_10158_yuyoung_fail.java

--- 00_기초문제/BOJ/10158/src/boj_10158_yuyoung_fail.java
+++ 00_기초문제/BOJ/10158/src/boj_10158_yuyoung_fail.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class boj_10158_yuyoung {
+public class boj_10158_yuyoung_fail {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
