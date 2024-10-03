51350363ea2587f5612ec58982d5aefcdb9ff00c
hyenem <gpals04291@gmail.com>
2024-10-04 08:01:31 +0900

    1004

--- 00_기초문제/PRGMS/PRGMS_340213_동영상재생기/prgrm_340213_hyenem.java
+++ 00_기초문제/PRGMS/PRGMS_340213_동영상재생기/prgrm_340213_hyenem.java
@@ -5,29 +5,31 @@ class Solution {
         int posSec = (pos.charAt(0)*10+pos.charAt(1))*60+(pos.charAt(3)*10 + pos.charAt(4))-dis;
         int opStartSec = (op_start.charAt(0)*10+op_start.charAt(1))*60+(op_start.charAt(3)*10 + op_start.charAt(4))-dis;
         int opEndSec = (op_end.charAt(0)*10+op_end.charAt(1))*60+(op_end.charAt(3)*10 + op_end.charAt(4))-dis;
-        
+
+        if(posSec>=opStartSec && posSec<=opEndSec){
+            posSec = opEndSec;
+        }
         for(int i = 0; i<commands.length; i++){
             if(commands[i].equals("next")) {
                 posSec = Math.min(posSec+10, endSec);
-            } else if (commands[i].equals("prev")){
-                posSec = Math.max(0, posSec-10);
             } else {
-                if(posSec>=opStartSec && posSec<=opEndSec){
-                    posSec = opEndSec;
-                }
+                posSec = Math.max(0, posSec-10);
+            }
+            if(posSec>=opStartSec && posSec<=opEndSec){
+                posSec = opEndSec;
             }
         }
         String ans = "";
         ans += posSec/600;
         posSec = posSec%600;
-        
+
         ans += posSec/60;
         posSec = posSec%60;
         ans+=":";
-        
+
         ans += posSec/10;
         ans += posSec%10;
-        
+
         return ans;
     }
 }
